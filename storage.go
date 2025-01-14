ebeee75db194106fe05f254af568ac6fd2777597
Zulfugar Abdullayev <zulfizupy@gmail.com>
2025-01-14 15:37:53 +0200

    refactor: move EnsureStoragefile functiion to storage.go

--- storage.go
+++ storage.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"os"
 )
 
@@ -9,7 +10,23 @@ type Storage[T any] struct {
 	FileName string
 }
 
-func NewStorage[T any] (fileName string) *Storage[T] {
+func EnsureStorageFile[T any](storage *Storage[T], data T) error {
+	dataPath := fmt.Sprintf("./%s", storage.FileName)
+
+	if _, err := os.Stat(dataPath); err != nil {
+		if os.IsNotExist(err) {
+			fmt.Println("creating file", storage.FileName)
+			return storage.Save(data)
+		}
+		fmt.Println("Error accessing file:", err)
+		return err
+	}
+
+	fmt.Printf("file exists %s, no action needed.\n", storage.FileName)
+	return nil
+}
+
+func NewStorage[T any](fileName string) *Storage[T] {
 	return &Storage[T]{FileName: fileName}
 }
 
@@ -21,7 +38,7 @@ func (storage *Storage[T]) Save(data T) error {
 	}
 
 	return os.WriteFile(storage.FileName, fileData, 0644)
-} 
+}
 
 func (storage *Storage[T]) Load(data *T) error {
 	fileData, err := os.ReadFile(storage.FileName)
