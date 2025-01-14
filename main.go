62ac2031d467d85004652fb571a7fec7884fcbea
Zulfugar Abdullayev <zulfizupy@gmail.com>
2025-01-14 16:38:58 +0200

    refactor: setup server configuration from api.go and move EnsureStorageFile to storage.go

--- main.go
+++ main.go
@@ -2,26 +2,7 @@ package main
 
 import (
 	"fmt"
-	"os"
 )
-
-func ensureStorageFile[T any](storage *Storage[T], data T) error {
-	dataPath := fmt.Sprintf("./%s", storage.FileName)
-	
-	if _, err := os.Stat(dataPath); err != nil {
-		if os.IsNotExist(err) {
-			fmt.Println("creating file", storage.FileName)
-			storage.Save(data)
-			return nil
-		}
-		fmt.Println("Error accessing file:", err)
-		return err
-	}
-
-	fmt.Printf("file exists %s, no action needed.\n", storage.FileName)
-	return nil
-}
-
 
 func main() {
 	fmt.Println("RWAPIGolang runs...")
@@ -29,8 +10,11 @@ func main() {
 	vehicles := Vehicles{}
 
 	customerStorage := NewStorage[Customers]("customers.json")
-	ensureStorageFile(customerStorage, customers)
+	EnsureStorageFile(customerStorage, customers)
 
 	vehicleStorage := NewStorage[Vehicles]("vehicles.json")
-	ensureStorageFile(vehicleStorage, vehicles)
+	EnsureStorageFile(vehicleStorage, vehicles)
+
+	server := NewAPIServer(":8080", customerStorage, vehicleStorage)
+	server.Run()
 }
