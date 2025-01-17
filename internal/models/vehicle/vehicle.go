02bd2fb825f87d2fe045e435be44b7f38a1ba015
Zulfugar Abdullayev <zulfizupy@gmail.com>
2025-01-17 07:00:22 +0200

    refactor: move vehicle.go file into internal/models/vehicle folder

---
+++ internal/models/vehicle/vehicle.go
@@ -0,0 +1,14 @@
+package vehicle
+
+type Vehicle struct {
+	PlateNumber string
+	Make        string
+	Model       string
+	Year        int
+	FuelType    string
+	Gearbox     string
+	Color       string
+	Body        string
+}
+
+type Vehicles map[string]Vehicle
