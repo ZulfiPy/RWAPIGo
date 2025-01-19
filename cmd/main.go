c0c53935074496a6180904cefa3564307166c29e
Zulfugar Abdullayev <zulfizupy@gmail.com>
2025-01-19 12:35:22 +0200

    refactor: delete vehicle related variables

--- cmd/main.go
+++ cmd/main.go
@@ -5,22 +5,16 @@ import (
 
 	"github.com/ZulfiPy/RWAPIGo/internal/api"
 	"github.com/ZulfiPy/RWAPIGo/internal/models/customer"
-	"github.com/ZulfiPy/RWAPIGo/internal/models/vehicle"
 	"github.com/ZulfiPy/RWAPIGo/internal/storage"
 )
 
 func main() {
 	fmt.Println("RWAPIGolang runs...")
 	customers := customer.Customers{}
-	vehicles := vehicle.Vehicles{}
 
-	// customerStorage := storage.NewStorage[customer.Customers]("customers.json")
 	customerStorage := customer.NewCustomerStorage("customers.json")
 	storage.EnsureStorageFile(customerStorage.GetStorage(), customers)
 
-	vehicleStorage := storage.NewStorage[vehicle.Vehicles]("vehicles.json")
-	storage.EnsureStorageFile(vehicleStorage, vehicles)
-
-	server := api.NewAPIServer(":8080", customerStorage, vehicleStorage)
+	server := api.NewAPIServer(":8080", customerStorage)
 	server.Run()
 }
