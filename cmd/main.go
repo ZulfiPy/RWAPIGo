0641b348692eea713bc9577278f903ce146dbe98
Zulfugar Abdullayev <zulfizupy@gmail.com>
2025-01-20 06:29:30 +0200

    refactor: provide vehicleStorage into NewAPIServer

--- cmd/main.go
+++ cmd/main.go
@@ -5,16 +5,22 @@ import (
 
 	"github.com/ZulfiPy/RWAPIGo/internal/api"
 	"github.com/ZulfiPy/RWAPIGo/internal/models/customer"
+	"github.com/ZulfiPy/RWAPIGo/internal/models/vehicle"
 	"github.com/ZulfiPy/RWAPIGo/internal/storage"
 )
 
 func main() {
 	fmt.Println("RWAPIGolang runs...")
+
 	customers := customer.Customers{}
+	vehicles := vehicle.Vehicles{}
 
 	customerStorage := customer.NewCustomerStorage("customers.json")
 	storage.EnsureStorageFile(customerStorage.GetStorage(), customers)
 
-	server := api.NewAPIServer(":8080", customerStorage)
+	vehicleStorage := vehicle.NewVehicleStorage("vehicles.json")
+	storage.EnsureStorageFile(vehicleStorage.GetStorage(), vehicles)
+
+	server := api.NewAPIServer(":8080", customerStorage, vehicleStorage)
 	server.Run()
 }
