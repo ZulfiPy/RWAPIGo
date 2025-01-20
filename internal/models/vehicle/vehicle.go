b790d721fd43ade1abfd6684e0edaad0fcb02979
Zulfugar Abdullayev <zulfizupy@gmail.com>
2025-01-20 06:24:51 +0200

    feat: initialize vehicle package

---
+++ internal/models/vehicle/vehicle.go
@@ -0,0 +1,32 @@
+package vehicle
+
+import (
+	"github.com/ZulfiPy/RWAPIGo/internal/storage"
+)
+
+type Vehicle struct {
+	PlateNumber string
+	Make string
+	Model string
+	Year int
+	FuelType string
+	Gearbox string
+	Color string
+	Body string
+}
+
+type Vehicles map[string]Vehicle
+
+type VehicleStorage struct {
+	storage *storage.Storage[Vehicles]
+}
+
+func NewVehicleStorage(fileName string) *VehicleStorage {
+	return &VehicleStorage{
+		storage: storage.NewStorage[Vehicles](fileName),
+	}
+}
+
+func (vs *VehicleStorage) GetStorage() *storage.Storage[Vehicles] {
+	return vs.storage
+}
