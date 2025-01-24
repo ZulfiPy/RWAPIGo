8f3525d8e02583de63c87549ccf5c5a19757f5af
Zulfugar Abdullayev <zulfizupy@gmail.com>
2025-01-24 07:52:21 +0200

    feature: initialize employee storage setup

---
+++ internal/models/employee/employee.go
@@ -0,0 +1,33 @@
+package employee
+
+import (
+	"time"
+
+	"github.com/ZulfiPy/RWAPIGo/internal/storage"
+)
+
+type Employee struct {
+	FirstName   string
+	LastName    string
+	PersonalID  int64
+	DateOfBirth time.Time
+	Email       string
+	PhoneNumber string
+	Address     string
+}
+
+type Employees []Employee
+
+type EmployeeStorage struct {
+	storage *storage.Storage[Employees]
+}
+
+func NewEmployeeStorage(fileName string) *EmployeeStorage {
+	return &EmployeeStorage{
+		storage: storage.NewStorage[Employees](fileName),
+	}
+}
+
+func (es *EmployeeStorage) GetStorage() *storage.Storage[Employees] {
+	return es.storage
+}
